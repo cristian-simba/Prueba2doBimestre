14f80e411feb6d922ecc511b45a638227c1511af
cristian-simba <davidsimba200@gmail.com>
2023-07-14 12:23:27 -0500

    Prueba

--- src/Cuestionario.java
+++ src/Cuestionario.java
@@ -1,2 +1,136 @@
-package PACKAGE_NAME;public class Cuestionario {
+import javax.swing.*;
+import java.io.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class Cuestionario {
+    private JPanel cuestionario;
+    private JLabel codLabel;
+    private JTextField codigo;
+    private JLabel cedLabel;
+    private JTextField cedula;
+    private JLabel nomLabel;
+    private JTextField nombre;
+    private JLabel apeLabel;
+    private JTextField apellido;
+    private JLabel fechaLabel;
+    private JComboBox anioCombo;
+    private JComboBox mesCombo;
+    private JComboBox diaCombo;
+    private JLabel colorLabel;
+    private JCheckBox rojoCheckBox;
+    private JCheckBox verdeCheckBox;
+    private JCheckBox ningunoCheckBox;
+    private JLabel casadoLabel;
+    private JRadioButton siRadioButton;
+    private JRadioButton noRadioButton;
+    private JLabel signoLabel;
+    private JComboBox signoCombo;
+    private JButton atrBt;
+    private JButton sigBt;
+    private JButton guardarBt;
+    private JButton cargarBt;
+    private ArrayList<Estudiantes> estudiantesDatos;
+    int ind;
+
+    public Cuestionario() {
+        estudiantesDatos = new ArrayList<>();
+        guardarBt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int codigoIngr = Integer.parseInt(codigo.getText());
+                int cedulaIngr = Integer.parseInt(cedula.getText());
+                String nombreIngr = nombre.getText();
+                String apellidoIngr = apellido.getText();
+                String signoIngr = (String) signoCombo.getSelectedItem();
+                String anioIngr = (String) anioCombo.getSelectedItem();
+                String mesIngr = (String) mesCombo.getSelectedItem();
+                String diaIngr = (String) diaCombo.getSelectedItem();
+                String colorIngr = "Color: ";
+                if(rojoCheckBox.isSelected()){
+                    colorIngr += "Rojo";
+                }
+                if(verdeCheckBox.isSelected()){
+                    colorIngr += "Verde";
+                }
+                if(ningunoCheckBox.isSelected()){
+                    colorIngr += "Ninguno";
+                }
+
+
+                Estudiantes ingreso = new Estudiantes(codigoIngr,cedulaIngr,nombreIngr,apellidoIngr,signoIngr,anioIngr,mesIngr,diaIngr,"","");
+                estudiantesDatos.add(ingreso);
+
+                for(int i = 0; i < estudiantesDatos.size(); i++){
+                    String filePath = i + "data.dat";
+                    try(
+                        FileOutputStream fileOut = new FileOutputStream(filePath);
+                        ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+                    ){
+                        objOut.writeObject(ingreso);
+                        System.out.println("Archivo guardado correctamente");
+                    }
+                    catch (IOException ex){
+                        throw new RuntimeException(ex);
+                    }
+                }
+                codigo.setText("");
+                cedula.setText("");
+                nombre.setText("");
+                apellido.setText("");
+                signoCombo.setSelectedItem("Escoge tu signo");
+                anioCombo.setSelectedItem("año");
+                mesCombo.setSelectedItem("mes");
+                diaCombo.setSelectedItem("dia");
+            }
+        });
+
+        // Boton de Atras
+        atrBt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(ind>0){
+                    ind--;
+                    Estudiantes estudiantes = estudiantesDatos.get(ind);
+                    codigo.setText(String.valueOf(estudiantes.getCodigo()));
+                    cedula.setText(String.valueOf(estudiantes.getCedula()));
+                    nombre.setText(estudiantes.getNombre());
+                    apellido.setText(estudiantes.getApellido());
+                    signoCombo.setSelectedItem(estudiantes.getSigno());
+                    anioCombo.setSelectedItem(estudiantes.getAnio());
+                    mesCombo.setSelectedItem(estudiantes.getMes());
+                    diaCombo.setSelectedItem(estudiantes.getDia());
+
+                }
+            }
+        });
+
+        // Boton Siguiente
+        sigBt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(ind < estudiantesDatos.size()-1){
+                    ind++;
+                    Estudiantes estudiantes = estudiantesDatos.get(ind);
+                    codigo.setText(String.valueOf(estudiantes.getCodigo()));
+                    cedula.setText(String.valueOf(estudiantes.getCedula()));
+                    nombre.setText(estudiantes.getNombre());
+                    apellido.setText(estudiantes.getApellido());
+                    signoCombo.setSelectedItem(estudiantes.getSigno());
+                    anioCombo.setSelectedItem(estudiantes.getAnio());
+                    mesCombo.setSelectedItem(estudiantes.getMes());
+                    diaCombo.setSelectedItem(estudiantes.getDia());
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Estudiantes");
+        frame.setContentPane(new Cuestionario().cuestionario);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
--- src/Estudiantes.java
+++ src/Estudiantes.java
@@ -1,2 +1,118 @@
-package PACKAGE_NAME;public class Estudiantes {
+import java.io.Serializable;
+
+public class Estudiantes implements Serializable {
+    int codigo;
+    int cedula;
+    String nombre;
+    String apellido;
+    String signo;
+    String anio;
+    String mes;
+    String dia;
+    String color;
+    String casado;
+
+    public Estudiantes(int codigo, int cedula, String nombre, String apellido, String signo, String anio, String mes, String dia, String color, String casado) {
+        this.codigo = codigo;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.signo = signo;
+        this.anio = anio;
+        this.mes = mes;
+        this.dia = dia;
+        this.color = color;
+        this.casado = casado;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getSigno() {
+        return signo;
+    }
+
+    public void setSigno(String signo) {
+        this.signo = signo;
+    }
+
+
+    public void setAnio(String anio) {
+        this.anio = anio;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getCasado() {
+        return casado;
+    }
+
+    public String getAnio() {
+        return anio;
+    }
+
+    public String getMes() {
+        return mes;
+    }
+
+    public void setCasado(String casado) {
+        this.casado = casado;
+    }
+    public String toString(){
+        return "Código: "+codigo+
+                "/ Cédula: "+cedula+
+                "/ Nombres: "+nombre+
+                "/ Apellido: "+apellido+
+                "/ Signo: "+signo+
+                "/ Fecha de nacimiento: "+anio+"-"+mes+"-"+dia+
+                "/ Color Favorito: "+color+
+                "/ Casado: "+casado;
+    }
 }
